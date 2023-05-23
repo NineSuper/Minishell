90ef34fedbbf146b1240622680313ccb5953f4d9
ltressen <paloouf@gmail.com>
2023-05-23 10:33:58 +0200

    Added header

---
+++ minishell.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ltressen <ltressen@student.42perpignan.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/23 10:32:06 by ltressen          #+#    #+#             */
+/*   Updated: 2023/05/23 10:33:46 by ltressen         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/uio.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+# include <sys/signal.h>
+# include <errno.h>
+# include <string.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include "libft/libft/libft.h"
+# include "libft/ft_printf/ft_printf.h"
+# include "libavfilter/gnl/get_next_line.h"
+
+#endif
