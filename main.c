38432f51546bf9d9e22eaff485438ac92072dfc4
ltressen <paloouf@gmail.com>
2023-05-24 16:11:08 +0200

    clean

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ltressen <ltressen@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 10:05:15 by tde-los-          #+#    #+#             */
-/*   Updated: 2023/05/24 16:06:09 by ltressen         ###   ########.fr       */
+/*   Updated: 2023/05/24 16:10:57 by ltressen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,15 +17,14 @@ int	main(int argc, char **argv, char **env)
 	t_data	*data;
 	(void)argc;
 	(void)argv;
-	char *lol;
+	char *prompt;
 
-	
 	data = ft_calloc(1, sizeof(t_data));
 	while (1)
 	{
-		lol = readline("\033[1;36m➜ "NAME" >\033[0m ");
-		add_history(lol);
-		if (!ft_strncmp(lol, "pwd", 3))
+		prompt = readline("\033[1;36m➜ "NAME" >\033[0m ");
+		add_history(prompt);
+		if (!ft_strncmp(prompt, "pwd", 3))
 		{
 			ft_getpwd(data);
 			ft_printf("%s\n", data->pwd);
