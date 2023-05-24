9373c89c2812316134ba123aa0ed7333b89d587d
ltressen <paloouf@gmail.com>
2023-05-24 14:33:35 +0200

    Fix prompt

--- main.c
+++ main.c
@@ -6,25 +6,13 @@
 /*   By: ltressen <ltressen@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 10:05:15 by tde-los-          #+#    #+#             */
-/*   Updated: 2023/05/24 14:25:11 by ltressen         ###   ########.fr       */
+/*   Updated: 2023/05/24 14:32:45 by ltressen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-<<<<<<< HEAD
 int	main(int argc, char **argv, char **env)
-=======
-/*
-Todo list :
-! Alerte "!"
-? Question "?"
-* Vert "*"
-todo: "todo"
-*/
-
-int	main(int argc, char **argv)
->>>>>>> refs/remotes/origin/main
 {
 	int i=0;
 	char *lol;
