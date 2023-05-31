59a433738e9ae161d2048d136395d586d42c0ff9
Jordan Casadessus <jcasades@c3r3p1.42perpignan.fr>
2023-05-31 12:05:49 +0200

    unset work, still some bugs

--- minishell.h
+++ minishell.h
@@ -6,7 +6,7 @@
 /*   By: ltressen <ltressen@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 10:32:06 by ltressen          #+#    #+#             */
-/*   Updated: 2023/05/29 12:42:34 by ltressen         ###   ########.fr       */
+/*   Updated: 2023/05/31 11:19:48 by jcasades         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,12 +32,13 @@
 # include "libft/ft_printf/ft_printf.h"
 # include "libft/gnl/get_next_line.h"
 /*      NAME        */
-# define NAME "\033[1;36m☁️  Minichibre "
+# define NAME "\033[1;36m☁️  Minisheel "
 /*	STRUCTS	    */
 typedef struct s_data
 {
 	char *pwd;
 	char **env_cpy;
+	int	env_len;
 	char *home;
 }	t_data;
 
@@ -45,5 +46,12 @@ void	ft_getpwd(t_data *data);
 void	ft_getenv(t_data *data, char **env);
 void	ft_cd(t_data *data, char *prompt, char **env);
 void	ft_gethome(t_data *data);
+void	ft_parsing(t_data *data, char *prompt, char **env);
+void	ft_echo(t_data *data, char *prompt);
+void	ft_exit(t_data *data, char *prompt);
+void	ft_freesplit(char **split);
+void	ft_export(t_data *data, char *prompt);
+void	ft_unset(t_data *data, char *prompt);
+int	ft_lenvar(char *varName);
 
 #endif
---
+++ parse.c
@@ -0,0 +1,95 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ltressen <ltressen@student.42perpignan.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/29 14:38:43 by ltressen          #+#    #+#             */
+/*   Updated: 2023/05/31 11:13:29 by jcasades         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+void	ft_parsing(t_data *data, char *prompt, char **env)
+{
+	int	i;
+	char **parsed;
+
+	parsed = ft_split(prompt, ' ');
+	if (!parsed[0])
+		return ;
+	i = 0;
+	if (!ft_strncmp(parsed[0], "pwd", 4))
+	{
+		ft_getpwd(data);
+		ft_printf("%s\n", data->pwd);
+	}		
+	if (!ft_strncmp(parsed[0], "env", 4))
+	{
+		//ft_getenv(data, env);
+		while (data->env_cpy[i])
+			ft_printf("%s\n", data->env_cpy[i++]);
+		i = 0;
+	}
+	if (!ft_strncmp(parsed[0], "cd", 3))
+		ft_cd(data, prompt, env);
+	if (!ft_strncmp(parsed[0], "echo", 5))
+		ft_echo(data, prompt);
+	if (!ft_strncmp(parsed[0], "exit", 5))
+		ft_exit(data, prompt);
+	if (!ft_strncmp(parsed[0], "export", 7))
+		ft_export(data, prompt);
+	if (!ft_strncmp(parsed[0], "unset", 6))
+		ft_unset(data, prompt);
+	ft_freesplit(parsed);
+}
+
+void	ft_exit(t_data *data, char *prompt)
+{
+	ft_freesplit(data->env_cpy);
+	free(data);
+	exit(EXIT_SUCCESS);
+}
+
+void	ft_freesplit(char **split)
+{
+	int	i;
+
+	i = 0;
+	while(split[i])
+		free(split[i++]);
+	free(split);
+}
+
+void	ft_echo(t_data *data, char *prompt)
+{
+	int	i;
+	int	j;
+	char **split;
+	int	n_flag;
+
+	n_flag = 0;
+	i = 1;
+	split = ft_split(prompt, ' ');
+	while (!ft_strncmp(split[i], "-n", 3))
+	{
+		n_flag = 26;
+		i++;
+	}
+	while (split[i])
+	{
+		if (!split[i + 1])
+		{
+			ft_printf("%s", split[i]);
+			if (n_flag)
+				ft_printf("\033[47m\e[1;30m%%\033[m");
+			ft_printf("\n");
+		}
+		else
+			ft_printf("%s ", split[i]);
+		i++;
+	}
+	ft_freesplit(split);
+}
