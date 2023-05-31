3e5a34c8319aef77ccc2ff858b7a2c95122bd43d
Jordan Casadessus <jcasades@c3r3p1.42perpignan.fr>
2023-05-31 17:29:07 +0200

    invalid read in unset

--- ft_export.c
+++ ft_export.c
@@ -6,7 +6,7 @@
 /*   By: ltressen <ltressen@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 14:24:42 by ltressen          #+#    #+#             */
-/*   Updated: 2023/05/31 11:59:28 by jcasades         ###   ########.fr       */
+/*   Updated: 2023/05/31 17:27:26 by jcasades         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,22 +36,24 @@ void	ft_export(t_data *data, char *prompt)
 		}
 		if (!data->env_cpy[j])
 		{
-			ft_printf("ptdr\n");
 			data->env_len++;
-			new_cpy = malloc(sizeof(char *) * data->env_len + 1);
+			new_cpy = ft_calloc((data->env_len + 1), sizeof(char *));
 			j = 0;
 			while (data->env_cpy[j])
 			{
-				new_cpy[j] = data->env_cpy[j];
+				new_cpy[j] = ft_strdup(data->env_cpy[j]);
+				free (data->env_cpy[j]);
 				j++;
 			}
 			free(data->env_cpy);
-			new_cpy[j] = sp[i];
-			new_cpy[++j] = NULL;
+			new_cpy[j] = ft_strdup(sp[i]);
+			free(sp[i]);
 			data->env_cpy = new_cpy;
 		}
 		i++;
 	}
+	free(sp[0]);
+	free(sp);
 }
 
 void	ft_unset(t_data *data, char *prompt)
@@ -62,46 +64,45 @@ void	ft_unset(t_data *data, char *prompt)
 	int	j;
 	int	k;
 
-	j = 0;
 	sp = ft_split(prompt, ' ');
 	i = 1;
 	while (sp[i])
-	{
+	{	
+		j = 0;
 		while (data->env_cpy[j])
 		{
 			if (!strncmp(data->env_cpy[j], sp[i], ft_lenvar(sp[i])))
-			{
-				ft_printf("ptdr2\n");
 				break ;
-			}
 			j++;
 		}
 		if (data->env_cpy[j])
-		{
-			ft_printf("ptdr\n");
-			new_cpy = malloc(sizeof(char *) * data->env_len);
+		{	
+			data->env_len--;
+			new_cpy = ft_calloc((data->env_len + 1), sizeof(char *));
 			j = 0;
 			k = 0;
 			while (data->env_cpy[j])
 			{
 				if (!strncmp(data->env_cpy[j], sp[i], ft_lenvar(sp[i])))
-				{
-					ft_printf("ptdr3\n");
-					free(data->env_cpy[j]);
-					ft_printf("ptdr3\n");
+//				{
+//					free(data->env_cpy[j]);
 					j++;
+//				}
+				if (data->env_cpy[j])
+				{
+					new_cpy[k] = ft_strdup(data->env_cpy[j]);
+//					free (data->env_cpy[j]);
 				}
-				ft_printf("%s\n", data->env_cpy[j]);
-				new_cpy[k] = data->env_cpy[j];
 				j++;
 				k++;
 			}
-			ft_printf("ptdr3\n");
-			free(data->env_cpy);
-			new_cpy[++k] = NULL;
+//			free(data->env_cpy);
 			data->env_cpy = new_cpy;
 		}
 		i++;
+		ft_printf("%d", data->env_len);
+//		free(sp[0]);
+//		free(sp);
 	}
 }
 
