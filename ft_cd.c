825831804c12e31f53b40bd267bb630b2284cb80
Tom De Los Muros <tom.delosmuros@gmail.com>
2023-06-07 12:32:01 +0200

    fix env

--- ft_cd.c
+++ ft_cd.c
@@ -6,19 +6,40 @@
 /*   By: tde-los- <tde-los-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 12:06:22 by ltressen          #+#    #+#             */
-/*   Updated: 2023/06/06 15:30:58 by tde-los-         ###   ########.fr       */
+/*   Updated: 2023/06/07 12:31:39 by tde-los-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+void	ft_changepwd(t_data *data, char *old_pwd)
+{
+	char	*str;
+	int		i;
+
+	i = 0;
+	while (ft_strncmp(data->env_cpy[i], "PWD", 3))
+		i++;
+	ft_getpwd(data);
+	str = ft_strjoin("PWD=", data->pwd);
+	ft_strlcpy(data->env_cpy[i], str, ft_strlen(str) + 1);
+	while (ft_strncmp(data->env_cpy[i], "OLDPWD", 6))
+		i++;
+	str = ft_strjoin("OLDPWD=", old_pwd);
+	ft_strlcpy(data->env_cpy[i], str, ft_strlen(str) + 1);
+	free(old_pwd);
+	free(str);
+}
+
 void	ft_cd(t_data *data, char *prompt, char **env)
 {
 	char	**parse;
+	char	*pwd_copy;
 
 	parse = ft_split(prompt, ' ');
 	ft_gethome(data);
 	ft_getpwd(data);
+	pwd_copy = ft_strdup(data->pwd);
 	if (!ft_strncmp(parse[0], "cd", 3));
 	{
 		if (parse[1] && parse[1][0] != '~')
@@ -32,7 +53,10 @@ void	ft_cd(t_data *data, char *prompt, char **env)
 			if (parse[1])
 			{
 				if (parse[1][1] == '\0')
+				{
+					ft_changepwd(data, pwd_copy);
 					return ;
+				}
 				chdir(data->home);
 				parse[1] += 2;
 				if (chdir(parse[1]) == -1)
@@ -43,4 +67,5 @@ void	ft_cd(t_data *data, char *prompt, char **env)
 			}
 		}
 	}
+	ft_changepwd(data, pwd_copy);
 }
