fa504c7bafcfbb42d68f576dec5b7ff5febeb799
Alexandre Saez <alsaez@c3r3p3.42perpignan.fr>
2023-06-14 16:15:43 +0200

    trying to fix pipes

--- new_parse.c
+++ new_parse.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/* ************************************************************************* */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   new_parse.c                                        :+:      :+:    :+:   */
@@ -11,6 +11,124 @@
 /* ************************************************************************** */
 
 #include "minishell.h"
+
+void	ft_execve(t_data *data, int i)
+{
+	char	*cmd;
+	//pid_t	pid;
+
+	//if (pid == 0)
+	//{
+		cmd = ft_chk_cmd(data, i);
+		if (cmd)
+		{	
+			if (execve(cmd, ft_split(data->cmd_full[i], ' '), data->env_cpy) == -1)
+				ft_printf("%d, %s\n", errno, strerror(errno));
+		//	exit(1);
+		}
+		else
+			exit(EXIT_SUCCESS) ;
+	//}
+}
+
+void	ft_exec(t_data *data, int i)
+{
+	int	j;
+
+	j = 0;
+	if (!data->cmd[0])
+		exit(1) ;
+	if (!ft_strncmp(data->cmd[i], "cd", 3))
+	{
+		ft_cd(data, data->cmd_full[i], data->env_cpy);
+		exit(1);
+	}
+	else if (!ft_strncmp(data->cmd[i], "echo", 5))
+	{
+		ft_echo(data, data->cmd_full[i]);
+		exit(1);
+	}
+	else if (!ft_strncmp(data->cmd[i], "export", 7))
+	{
+		ft_export(data, data->cmd_full[i]);
+		exit(1);
+	}
+	else if (!ft_strncmp(data->cmd[i], "unset", 6))
+	{
+		ft_unset(data, data->cmd_full[i]);
+		exit(1);
+	}
+	else if (!ft_strncmp(data->cmd[i], "env", 4))
+	{
+		while (data->env_cpy[j])
+			ft_printf("%s\n", data->env_cpy[j++]);
+		exit(1);
+	}
+	else if (!ft_strncmp(data->cmd[i], "pwd", 4))
+	{
+		ft_getpwd(data);
+		ft_printf("%s\n", data->pwd);
+		exit(1);
+	}		
+	else if (!ft_strncmp(data->cmd[i], "exit", 5))
+	{
+		
+		ft_exit(data, NULL);
+		kill(0, SIGKILL);
+	}
+	else
+		ft_execve(data, i);
+}
+/*void	is_builtin(t_data *data, char *cmd, int i)
+{
+	if (!ft_strncmp(cmd, "cd", 3))
+		data->builtin[i] = 1;
+	else if (!ft_strncmp(cmd, "echo", 5))
+		data->builtin[i] = 1;
+	else if (!ft_strncmp(cmd, "export", 7))
+		data->builtin[i] = 1;
+	else if (!ft_strncmp(cmd, "unset", 6))
+		data->builtin[i] = 1;
+	else if (!ft_strncmp(cmd, "env", 4))
+		data->builtin[i] = 1;
+	else if (!ft_strncmp(cmd, "pwd", 4))
+		data->builtin[i] = 1;
+	else if (!ft_strncmp(cmd, "exit", 5))
+		data->builtin[i] = 1;
+	else
+		data->builtin[i] = 0;
+}
+*/
+static void ft_pipe(t_data *data, int n)
+{
+	pid_t	pid;
+	int		i;
+
+	i = -1;
+	pid = fork();
+	while (++i < n)
+	{
+		if (pid == 0)
+		{
+			if (i == 0)
+			{
+				dup2(data->fd1, 0);
+				dup2(data->pipes[0][1], 1);
+			}
+			else if (i == data->pipenum - 1) 
+			{
+				dup2(data->pipes[(i + 1) % 2][0], 0);
+				dup2(data->fd2, 1);	
+			}
+			else
+			{
+				dup2(data->pipes[(i + 1) % 2][0], 0);
+				dup2(data->pipes[i % 2][1], 1);
+			}
+			ft_exec(data, i);
+		}
+	}
+}
 
 void	ft_first_parse(t_data *data, char *prompt)
 {
@@ -30,16 +148,19 @@ void	ft_first_parse(t_data *data, char *prompt)
 		tmp = ft_split(data->cmd_full[i], ' ');
 		if (tmp[0])
 			data->cmd[i] = ft_strdup(tmp[0]);
-		j = 0;
-		while (tmp[j])
-		{
-			free(tmp[j]);
-			j++;
-		}
-		free (tmp);
+		ft_freesplit(tmp);
 		i++;
 	}
+//	data->builtin = malloc((i + 1) * 4);
+//	i = 0;
+//	while (data->cmd[i])
+//	{
+//		is_builtin(data, data->cmd[i], i);
+//		i++;
+//	}
+	//free(data->builtin);
 }
+
 
 void	ft_second_parse(t_data *data)
 {
@@ -79,104 +200,103 @@ char	*ft_chk_cmd(t_data *data, int i)
 	ft_freesplit(spt);
 	return (NULL);
 }
-		
-void	ft_execve(t_data *data, int i)
-{
-	char	*cmd;
-	pid_t	pid;
-
-	cmd = ft_chk_cmd(data, i);
-	if (cmd)
-	{
-		pid = fork();
-		if (pid == 0)
-		{
-			if (execve(cmd, ft_split(data->cmd_full[i], ' '), data->env_cpy) == -1)
-				ft_printf("%d, %s\n", errno, strerror(errno));
-		}
-	}
-	else
-		return ;
-}
-
-void	ft_exec(t_data *data, int i)
-{
-	int	j;
-
-	j = 0;
-	if (!data->cmd[0])
-		return ;
-	if (!ft_strncmp(data->cmd[i], "cd", 3))
-		ft_cd(data, data->cmd_full[i], data->env_cpy);
-	else if (!ft_strncmp(data->cmd[i], "echo", 5))
-		ft_echo(data, data->cmd_full[i]);
-	else if (!ft_strncmp(data->cmd[i], "export", 7))
-		ft_export(data, data->cmd_full[i]);
-	else if (!ft_strncmp(data->cmd[i], "unset", 6))
-		ft_unset(data, data->cmd_full[i]);
-	else if (!ft_strncmp(data->cmd[i], "env", 4))
-	{
-		while (data->env_cpy[j])
-			ft_printf("%s\n", data->env_cpy[j++]);
-	}
-	else if (!ft_strncmp(data->cmd[i], "pwd", 4))
-	{
-		ft_getpwd(data);
-		ft_printf("%s\n", data->pwd);
-	}		
-	else if (!ft_strncmp(data->cmd[i], "exit", 5))
-		ft_exit(data, NULL);
-	else
-		ft_execve(data, i);
-	i++;
-}
 
 void	ft_piping(t_data *data, int i)
 {
 	pid_t	pid;
-
+	
 	pid = fork();
 	if (pid == 0)
 	{
 		if (i == 0)
 		{
 			dup2(data->fd1, 0);
-			dup2(data->pipes[i % 2][0], 1);
+			dup2(data->pipes[0][1], 1);
 		}
 		else if (i == data->pipenum - 1) 
 		{
-			dup2(data->pipes[i % 2][1], 0);
-			dup2(data->fd2, 1);
+			dup2(data->pipes[(i + 1) % 2][0], 0);
+			dup2(data->fd2, 1);	
 		}
 		else
 		{
-			dup2(data->pipes[i % 2][1], 0);
-			dup2(data->pipes[(i + 1) % 2][0], 1);
+			dup2(data->pipes[(i + 1) % 2][0], 0);
+			dup2(data->pipes[i % 2][1], 1);
 		}
+		close(data->pipes[i % 2][1]);
+		close(data->pipes[(i + 1) % 2][0]);
+		close(data->pipes[0][1]);
+		close(data->pipes[i % 2][0]);		
 		ft_exec(data, i);	
 	}
 }
+
+void	single_cmd(t_data *data)
+{
+	pid_t	pid;
+
+	pid = fork();
+	if (pid == 0)
+		ft_exec(data, 0);
+	wait(NULL);
+}
+/*void	delete_tmpfile(char *file, t_data *data)
+{
+	char	**argv;
+	pid_t	pid;
+
+	argv = malloc(sizeof(char *) * 3);
+	argv[0] = "/bin/rm";
+	ft_getpwd(data);
+	argv[1] = ft_strjoin(ft_strjoin(data->pwd, "/"), file);
+	argv[2] = NULL;
+	pid = fork();
+	if (pid == 0)
+		execve("/bin/rm", argv, NULL);
+}*/
 
 void	ft_parsingg(t_data *data, char *prompt)
 {
 	int	i;
 	int	fd;
-	
+	char	*line;
 	i = 0;
 	ft_first_parse(data, prompt);
 	ft_second_parse(data);
-	data->fd1 = open(".tmp1", O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
-	data->fd2 = open(".tmp2", O_CREAT | O_TRUNC | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	data->fd1 = open(".tmp1", O_CREAT | O_APPEND | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	data->fd2 = open(".tmp2", O_CREAT | O_WRONLY, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+	if (data->pipenum == 1 && !ft_strncmp(data->cmd[0], "exit", 5))
+		ft_exit(data, NULL);
 	if (data->pipenum == 1)
-		ft_exec(data, 0);
+		single_cmd(data);
 	else
 	{
-		while (data->cmd_full[i])
+		ft_pipe(data, data->pipenum);
+		/*while (data->cmd_full[i])
 		{
 			ft_piping(data, i);
 			i++;
 		}
+		close(data->fd2);
+		data->fd2 = open(".tmp2", O_RDONLY);
+		line = get_next_line(data->fd2);
+		while (line)
+		{
+			ft_printf("%s", line);
+			line = get_next_line(data->fd2);
+		}
+		close(data->fd2);
+		data->fd2 = open(".tmp2", O_TRUNC | O_WRONLY);
+		close(data->pipes[0][0]);
+		close(data->pipes[0][1]);
+		close(data->pipes[1][0]);
+		close(data->pipes[1][1]);
+		close(data->fd1);
+		close(data->fd2); */
 	}
+	
 	ft_freesplit(data->cmd_full);
 	ft_freesplit(data->cmd);
 }
+
+
