a0371284942ed8b2d6163647a0182b4a03a3b745
Tom De Los Muros <tde-los-@c2r3p4.42perpignan.fr>
2023-06-27 12:30:30 +0200

    fix home && ft_cd.c

--- ft_env.c
+++ ft_env.c
@@ -6,7 +6,7 @@
 /*   By: tde-los- <tde-los-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/25 09:31:42 by ltressen          #+#    #+#             */
-/*   Updated: 2023/06/27 11:47:30 by tde-los-         ###   ########.fr       */
+/*   Updated: 2023/06/27 12:29:40 by tde-los-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,8 +36,9 @@ void	ft_gethome(t_data *data)
 
 	i = -1;
 	split = ft_split(data->pwd, '/');
-	data->home = ft_calloc(1, sizeof(char) + 1);
-	data->home[0] = '/';
+	if (data->home)
+		free (data->home);
+	data->home = "/";
 	while (split[++i] && i < 3)
 	{
 		data->home = ft_strjoin(data->home, split[i]);
