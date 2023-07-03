26286fb8d1cb75d15e35c8c5d47a09a6848c34b8
Jordan Casadessus <jcasades@c3r3p1.42perpignan.fr>
2023-07-03 14:25:24 +0200

    second phase of norminette

--- ft_reparg.c
+++ ft_reparg.c
@@ -6,7 +6,7 @@
 /*   By: ltressen <ltressen@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/22 16:35:29 by ltressen          #+#    #+#             */
-/*   Updated: 2023/06/30 11:03:41 by ltressen         ###   ########.fr       */
+/*   Updated: 2023/07/03 14:24:29 by jcasades         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,11 +25,7 @@ char	*ft_reparg(t_data *data, int i, int j)
 	k++;
 	while (data->cmd_full[i][k] != ' ' && data->cmd_full[i][k] != '"'
 		&& data->cmd_full[i][k] != '\'' && data->cmd_full[i][k])
-	{
-		res[j] = data->cmd_full[i][k];
-		k++;
-		j++;
-	}
+		res[j++] = data->cmd_full[i][k++];
 	res[j] = '\0';
 	j = 0;
 	while (data->env_cpy[j])
