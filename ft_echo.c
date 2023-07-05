c834c6cfaae09bdd824e7d5d9c04c88a06d1e2b1
Tom De Los Muros <tom.delosmuros@gmail.com>
2023-07-05 12:53:11 +0200

    finish minishell

--- ft_echo.c
+++ ft_echo.c
@@ -6,85 +6,52 @@
 /*   By: ltressen <ltressen@student.42perpignan.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/07 10:30:03 by tde-los-          #+#    #+#             */
-/*   Updated: 2023/06/30 10:25:59 by ltressen         ###   ########.fr       */
+/*   Updated: 2023/07/05 12:48:05 by jcasades         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	ft_check_n(char **tab)
+int	ft_check_n(char *prompt, int i)
 {
-	int	i;
-	int	j;
-
-	i = 1;
-	while (tab[i])
-	{
-		if (tab[i][0] != '-')
-			return (i);
-		j = 1;
-		while (tab[i][j])
-		{
-			if (tab[i][j] != 'n')
-				return (i);
-			j++;
-		}
+	while (prompt[i] == 'n')
 		i++;
-	}
-	return (i);
+	while (prompt[i] == ' ')
+		i++;
+	if (!(prompt[i] == '-' && prompt[i + 1] == 'n'))
+		return (i);
+	else
+		return (ft_check_n(prompt, i + 1));
 }
-/*
-todo echo $? = 0 si la commande passe est bonne sinon 127 si error
-*/
-
-// int	ft_arg_echo(t_data *data, char *arg)
-// {
-// 	int	i;
-// 	int	j;
-
-// 	i = 1;
-// 	j = 0;
-// 	while (arg[i] == '$')
-// 		i++;
-// 	if (i != 1)
-// 		return (ft_printf("%s ", arg + i));
-// 	arg++;
-// 	arg = ft_strjoin(arg, "=");
-// 	while (data->env_cpy[i])
-// 	{
-// 		if (!ft_strncmp(data->env_cpy[i], arg, ft_strlen(arg)))
-// 		{
-// 			while (data->env_cpy[i][j] != '=')
-// 				j++;
-// 			ft_printf("%s ", data->env_cpy[i] + j + 1);
-// 		}
-// 		i++;
-// 	}
-// }
 
 int	ft_echo(t_data *data, char *prompt)
 {
-	char		**split;
-	int			i;
-	int			flag;
+//	char		**split;
+//	int			i;
+//	int			flag;
 
 	(void)data;
-	split = ft_split(prompt, ' ');
-	if (!split[1])
-		return (ft_freesplit(split), ft_printf("\n"));
-	i = ft_check_n(split);
-	flag = i;
-	if (!split[i])
-		return (ft_freesplit(split), 0);
-	while (split[i])
-	{
-		if (split[i + 1])
-			ft_printf("%s ", split[i]);
-		else
-			ft_printf("%s", split[i]);
-		i++;
-	}
-	if (flag < 2)
-		ft_printf("\n");
-	return (ft_freesplit(split), 0);
+	if ((ft_strlen(prompt) > 7) && prompt[5] == '-' && prompt [6] == 'n')
+		ft_printf("%s", prompt + ft_check_n(prompt, 7));
+	else
+		ft_printf("%s\n", prompt + 5);
+//	split = ft_split(prompt, ' ');
+//	if (!split[1])
+//		return (ft_freesplit(split), ft_printf("\n"));
+//	i = ft_check_n(split);
+//	flag = i;
+//	if (!split[i])
+//		return (ft_freesplit(split), 0);
+//	while (split[i])
+//	{
+//		if (split[i + 1])
+//			ft_printf("%s ", split[i]);
+//		else
+//			ft_printf("%s", split[i]);
+//		i++;
+//	}
+//	if (flag < 2)
+//		ft_printf("\n");
+//	return (ft_freesplit(split), 0);
+	return (0);
 }
